fe18dea2a5eee00a9ad72daad45f9c3f748548f5
Anton <anton_hansson_@hotmail.se>
2023-03-09 10:27:39 +0100

    Skapat ett JSON objekt och skrivit och hämtat data via den

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import org.json.simple.JSONObject;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("This is an example");
+
+        //Skapa JSON objekt
+        JSONObject jsonOb = new JSONObject();
+
+        //Skapa värden. Först nyckelord sedan värde
+        jsonOb.put("namn", "Anton");
+        jsonOb.put("age", 29);
+
+        //Skriva ut värden
+        System.out.println("Mitt namn är " + jsonOb.get("namn"));
+        System.out.println("Jag är " + jsonOb.get("age") + " år gammal");
     }
 }
