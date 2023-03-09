bb320d5a6cd4eae64f3db34fa45acab1713edb13
Anton <anton_hansson_@hotmail.se>
2023-03-09 13:29:13 +0100

    dfgdfg

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -5,6 +6,9 @@ import org.json.simple.parser.ParseException;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException, ParseException {
@@ -22,11 +26,55 @@ public class Main {
         System.out.println("Mitt namn är " + jsonOb.get("namn"));
         System.out.println("Jag är " + jsonOb.get("age") + " år gammal");
 
-        Object o = new JSONParser().parse(new FileReader("src/data.json"));
-        JSONObject jsonData =(JSONObject) o;
+        fetchJsonFromFile();
+        fetchJsonFromAPI();
+    }
 
-        JSONObject person = (JSONObject) jsonData.get("p1");
-        String name = (String) person.get("name");
-        System.out.println("P1 Name :" + name);
+    static void fetchJsonFromFile() throws IOException, ParseException {
+
+        //Hämta data från JSON fil
+        JSONObject fetchData = (JSONObject) new JSONParser().parse(new FileReader("src/data.json"));
+
+        //Konvertera data till ett JSONObject
+        JSONObject person1 = (JSONObject) fetchData.get("p1");
+        JSONObject person2 = (JSONObject) fetchData.get("p2");
+
+        //Hämta och skriv ut data
+        String nameP1 = person1.get("name").toString();
+        String nameP2 = person2.get("name").toString();
+        int ageP1 = Integer.parseInt(person1.get("age").toString());
+        int ageP2 = Integer.parseInt(person2.get("age").toString());
+
+        System.out.println("P1 Name : " + nameP1);
+        System.out.println("P1 Age : " + ageP1);
+
+        JSONArray dataArray = (JSONArray) person1.get("data");
+        System.out.println(dataArray);
+        System.out.println(dataArray.get(2));
+    }
+
+    static void fetchJsonFromAPI() throws IOException, ParseException {
+        //Spara URL till API
+        URL url = new URL("https://api.wheretheiss.at/v1/satellites/25544");
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.connect();
+        if (conn.getResponseCode() == 200) {
+            System.out.println("Koppling lyckades");
+        } else {
+            System.out.println("Koppling misslyckades");
+        }
+
+        StringBuilder strData = new StringBuilder();
+        Scanner scanner = new Scanner(url.openStream());
+        while (scanner.hasNext()) {
+            strData.append(scanner.nextLine());
+        }
+        scanner.close();
+
+        JSONObject dataObject = (JSONObject) new JSONParser().parse(String.valueOf(strData));
+
+        System.out.println(dataObject);
     }
 }
